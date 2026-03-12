e7b75bc9797763d44e2cffc0f14e4269afd8a97a
Claudio Felipe <claudiolindo2@gmail.com>
2026-03-11 21:57:17 -0300

    adiciona codigos da aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+
+    printf("estou usando a funcao printf()\n");
+
+    printf("Programar em C eh TOP!\n");
+
+    printf("%011i\n", 10+10);
+    
+    printf("%.12f\n", 3.141592653589);
+
+    printf("%s", "Isso eh um string\n");
+
+    return 0;
+}
