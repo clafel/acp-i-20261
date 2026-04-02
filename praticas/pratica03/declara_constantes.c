cdca5b16ba3e39cdb5b004be02b3c1a4a7819817
Claudio Felipe <claudiolindo2@gmail.com>
2026-04-02 00:38:07 +0000

    feat: conclui pratica03. Fecha #ID

---
+++ praticas/pratica03/declara_constantes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define PI 3.14159265
+int main () {
+    
+    const double EULER = 2.71828182;
+
+    printf("O valor de Pi eh: %.8f\n", PI);
+    printf("O valor de Euler eh %.8f\n", EULER);
+
+    return 0;
+}
