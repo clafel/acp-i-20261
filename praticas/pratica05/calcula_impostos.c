758df806c556445dfdfe18b660f51c5398866f3d
Claudio Felipe <claudiolindo2@gmail.com>
2026-05-06 21:52:32 -0300

    feat: conclui pratica05. Fecha #ID

--- praticas/pratica05/calcula_impostos.c
+++ praticas/pratica05/calcula_impostos.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main () {
+    int numero = 0;
+    printf("Escreva o valor do produto: ");
+    scanf("%i", &numero);
 
+    float icms = (numero * 1.0f) * 0.17f;
+    float iss = (numero * 1.0f) * 0.05f;
+    float pis = (numero * 1.0f) * 0.0165f;
+    float valor_total = (1 + icms + iss + pis) * (numero *1.0f);
+
+    printf("O valor do imposto ICMS: %f\n", icms);
+     printf("O valor do imposto ISS: %f\n", iss);
+      printf("O valor do imposto PIS: %f\n", pis);
+       printf("O valor total: %f\n", valor_total);
     return 0;
 }
