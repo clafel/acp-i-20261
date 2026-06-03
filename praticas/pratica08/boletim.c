3de28be9adcd9bfbd85faff38227f7c7232cb900
Claudio Felipe <claudiolindo2@gmail.com>
2026-06-03 20:23:34 -0300

    feat: conclui pratica08. Fecha #ID

--- praticas/pratica08/boletim.c
+++ praticas/pratica08/boletim.c
@@ -3,12 +3,19 @@
 int main () {
     int i = 0;
     int b = 0;
-    int vetor[10][2];
+    float vetor[10][3];
     for (i = 0; i < 10; i++) {
+        printf("Informe a primeira nota para um dos alunos: \n");
+        scanf("%f", &vetor[i][0]);
+        printf("Informe a segunda nota para um dos alunos: \n");
+        scanf("%f", &vetor[i][1]);
 
-        for (b = 0; b < 2) {
-
-        }
+        vetor[i][2] = vetor[i][0] + vetor[i][1] / 2;
     }
+    
+    for (i = 0; i < 10; i++) {
+        printf("Aluno %i %5.1f %5.1f %5.1f\n", i+1, vetor[i][0], vetor[i][1], vetor[i][2]);
+    }
+
     return 0;
 }
